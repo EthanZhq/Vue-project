52979d532b247b9064e4434105a9495c57112e1b
EthanZ <ethanzhanghq@foxmail.com>
2019-11-28 11:48:57 +0800

    vant-store
    vant-ui项目

---
+++ vue-vant-store/README.md
@@ -0,0 +1,42 @@
+# vant-store
+一个商城Demo, 前端使用vant有赞UI,vue实现, 后端使用mysql,express实现。
+
+# 实现的功能
+
+1. 登录/注册
+2. 商品列表
+3. 商品详情
+4. 购物车
+5. 收货地址
+6. 订单
+
+# 前端
+
+## 项目运行
+
+``` bash
+# 安装依赖
+npm install
+
+# 开发预览
+npm run serve
+
+# 打包发布
+npm run build
+```
+
+# 后端
+
+## 项目运行
+
+``` bash
+# 进入目录
+cd server
+
+# 安装依赖
+npm install
+
+# 启动服务器
+npm start
+```
+
---
+++ vue-vant-store/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <keep-alive include="Home,Cart,User">
+      <router-view />
+    </keep-alive>
+    <Tab v-show="!$route.meta.fullScreen" />
+  </div>
+</template>
+
+<script>
+import Tab from '@/views/Tab/Tab';
+export default {
+  components: {
+    Tab,
+    Github
+  }
+};
+</script>
+<style lang="stylus">
+html, body, #app
+  width 100%
+  height 100%
+#github
+  position fixed
+  top 0
+  right 60px
+  z-index 999
+</style>
